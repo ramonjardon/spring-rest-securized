3a62d79d722c6f956fe4243ea47a8fb5fad6079c
Ramon Jardon <ramonjardon@gmail.com>
2018-07-19 00:52:30 +0200

    Version 0.0.1

--- spring-rest-securized-web/src/main/java/org/example/rest/controller/RestController.java
+++ spring-rest-securized-web/src/main/java/org/example/rest/controller/RestController.java
@@ -1,7 +1,11 @@
 package org.example.rest.controller;
 
+import java.security.Principal;
+
 import org.example.rest.model.Greeting;
 import org.example.rest.service.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +17,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value="/api")
 public class RestController {
 
+	private final static Logger LOG = LoggerFactory.getLogger(RestController.class);
 	@Autowired
 	private GreetingService service;
 	
 	@RequestMapping(value="/greeting",method=RequestMethod.GET)
-	public  @ResponseBody Greeting sayHello(@RequestParam(value="name") String name) {
+	public  @ResponseBody Greeting sayHello(@RequestParam(value="name") String name,Principal principal) {
+		if(null != principal && null != principal.getName()) {
+			LOG.info("User " + principal.getName());
+		}
 		return this.getService().sayHello(name);
 	}
 
